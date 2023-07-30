eec73cb6060bd67d4de66ebcf1a54f91d068c4e1
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-07-30 22:32:24 +0600

    first commit

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const run = async () => {
 run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello Worldsssssssssssss!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
