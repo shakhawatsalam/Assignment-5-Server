b3c814d4a9ace5470b88314b8434eb8df55165eb
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-07-30 21:14:47 +0600

    first commit

--- index.js
+++ index.js
@@ -18,215 +18,30 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db("library");
-    const bookCollection = db.collection("books");
-    const userCollection = db.collection("users");
+    const db = client.db("pcbuilder");
+    const componentsCollection = db.collection("components");
     // * Get All Books
 
-    app.get("/book/:id", async (req, res) => {
-      const id = req.params.id;
-      const book = await bookCollection.findOne({ _id: ObjectId(id) });
-      res.send({ status: true, data: book });
+    app.get("/components", async (req, res) => {
+      const cursor = await componentsCollection.find({});
+      const components = await cursor.toArray();
+
+      res.send({ status: true, data: components });
     });
 
-    app.post("/book/:id", async (req, res) => {
-      const bookId = req.params.id;
-      const updatedbook = req.body;
-
-      try {
-        const result = await bookCollection.updateOne(
-          { _id: ObjectId(bookId) },
-          { $set: updatedbook }
-        );
-
-        if (result.modifiedCount === 1) {
-          res.sendStatus(200);
-        } else {
-          res.sendStatus(404);
-        }
-      } catch (error) {
-        console.error(error);
-        res.sendStatus(500);
-      }
-    });
-
-    app.delete("/book/:id", async (req, res) => {
-      const id = req.params.id;
-
-      const result = await bookCollection.deleteOne({ _id: ObjectId(id) });
-      res.send(result);
-    });
-
-    app.get("/books", async (req, res) => {
-      const bookFilterableFields = [
-        "searchTerm",
-        "title",
-        "genre",
-        "author",
-        "publicationDate",
-      ];
-      const bookSearchFields = ["title", "author", "genre", "publicationDate"];
-      const pick = (obj, keys) => {
-        const finalObj = {};
-        for (const key of keys) {
-          if (obj && Object.hasOwnProperty.call(obj, key)) {
-            finalObj[key] = obj[key]; // Accessint thi value (page) => Ans: 1 value
-          }
-        }
-        return finalObj;
-      };
-      const filters = pick(req.query, bookFilterableFields);
-      const { searchTerm, ...filtersData } = filters;
-      const addConditions = [];
-      if (searchTerm) {
-        addConditions.push({
-          $or: bookSearchFields.map((field) => ({
-            [field]: {
-              $regex: searchTerm,
-              $options: "i",
-            },
-          })),
-        });
-      }
-
-      if (Object.keys(filtersData).length) {
-        addConditions.push({
-          $and: Object.entries(filtersData).map(([field, value]) => ({
-            [field]: value,
-          })),
-        });
-      }
-      const whereConditions =
-        addConditions.length > 0 ? { $and: addConditions } : {};
-
-      const cursor = await bookCollection.find(whereConditions).sort("dsc");
-      const book = await cursor.toArray();
-
-      res.send({ status: true, data: book });
-    });
-    app.post("/book", async (req, res) => {
-      const book = req.body;
-
-      const result = await bookCollection.insertOne(book);
+    app.get("/components/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await componentsCollection.findOne({ _id: ObjectId(id) });
 
       res.send(result);
     });
 
-    // * Comment Book
-    app.post("/reviews/:id", async (req, res) => {
-      const productId = req.params.id;
-      const reviews = req.body.reviews;
+    app.get("/components/featured/:category", async (req, res) => {
+      const { category } = req.params;
+      const cursor = await componentsCollection.find({ category: category });
+      const result = await cursor.toArray();
 
-      console.log(productId);
-      console.log(reviews);
-
-      const result = await bookCollection.updateOne(
-        { _id: ObjectId(productId) },
-        { $push: { reviews: reviews } }
-      );
-
-      console.log(result);
-
-      if (result.modifiedCount !== 1) {
-        console.error("Product not found or reviews not added");
-        res.json({ error: "Product not found or reviews not added" });
-        return;
-      }
-
-      console.log("Comment added successfully");
-      res.json({ message: "Comment added successfully" });
-    });
-
-    app.get("/reviews/:id", async (req, res) => {
-      const bookId = req.params.id;
-
-      const result = await bookCollection.findOne(
-        { _id: ObjectId(bookId) },
-        { projection: { _id: 0, reviews: 1 } }
-      );
-
-      if (result) {
-        res.json(result);
-      } else {
-        res.status(404).json({ error: "Product not found" });
-      }
-    });
-    // * Wish List
-    app.post("/wishlist/:email", async (req, res) => {
-      const email = req.params.email;
-      const wishList = req.body.wishlist;
-
-      console.log(email);
-      // console.log(reviews);
-
-      const result = await userCollection.updateOne(
-        { email: email },
-        { $push: { wishList: wishList } }
-      );
-
-      console.log(result);
-
-      if (result.modifiedCount !== 1) {
-        console.error("Product not found or reviews not added");
-        res.json({ error: "Product not found or reviews not added" });
-        return;
-      }
-
-      console.log("Comment added successfully");
-      res.json({ message: "Comment added successfully" });
-    });
-    app.post("/readinglist/:email", async (req, res) => {
-      const email = req.params.email;
-      const readingList = req.body.wishlist;
-
-      console.log(email);
-      // console.log(reviews);
-
-      const result = await userCollection.updateOne(
-        { email: email },
-        { $push: { readingList: readingList } }
-      );
-
-      console.log(result);
-
-      if (result.modifiedCount !== 1) {
-        console.error("Product not found or reviews not added");
-        res.json({ error: "Product not found or reviews not added" });
-        return;
-      }
-
-      console.log("Comment added successfully");
-      res.json({ message: "Comment added successfully" });
-    });
-
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      const cursor = await userCollection.find({ email: email });
-      const user = await cursor.toArray();
-
-      res.send({ status: true, data: user });
-    });
-
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      const { email } = user;
-      try {
-        const isExist = await userCollection.find({ email }).toArray();
-
-        if (isExist.length > 0) {
-          return res
-            .status(409)
-            .json({ error: "User with this email already exists" });
-        }
-
-        const result = await userCollection.insertOne(user);
-        res.json(result);
-      } catch (error) {
-        console.error("Error creating user:", error);
-        res
-          .status(500)
-          .json({ error: "An error occurred while creating the user" });
-      }
+      res.send({ status: true, data: result });
     });
   } finally {
   }
